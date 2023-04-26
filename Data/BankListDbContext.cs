d0f2f3a005b4e35577d9af7ebefe7539e2e63cc2
Sanmati Chougala <sanmati.chougala@sogeti.com>
2023-04-26 10:24:05 +0100

    Add entity framework core, Add some entities to DB context, Add Migration Create database Seeded data to Table

---
+++ Data/BankListDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankListAPI.VsCode.Data
+{
+    public class BankListDbContext : DbContext
+    {
+        public BankListDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        //created Enity classes Bank, Country & give it all props & then added just to DBSet below using DbSet.
+        //it will say these column exists in tables
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country { 
+                    Id = 1, 
+                    Name = "India", 
+                    ShortName = "IN" 
+                },
+                new Country { 
+                    Id = 2, 
+                    Name = "United Kingdom", 
+                    ShortName = "UK" 
+                }
+            );
+
+            modelBuilder.Entity<Bank>().HasData(
+                new Bank
+                {
+                    Id = 1,
+                    Name = "HDFC Bank",
+                    Address = "Trivandrum Kerala",
+                    Rating = 4.5,
+                    CountryId = 1
+                },
+                new Bank
+                {
+                    Id = 2,
+                    Name = "HSBC Bank",
+                    Address = "Leamingtion Spa",
+                    Rating = 4,
+                    CountryId = 2
+                },
+                new Bank
+                {
+                     Id = 3,
+                     Name = "Barclays",
+                     Address = "Leamingtion Spa",
+                     Rating = 4.5,
+                     CountryId = 2
+                },
+                new Bank
+                {
+                    Id = 4,
+                    Name = "SBI",
+                    Address = "Athani, India",
+                    Rating = 4.8,
+                    CountryId = 1
+                }
+            );
+        }
+    }
+}
---
+++ Migrations/20230425170321_SeededCountriesAndBanks.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BankListAPI.VsCode.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededCountriesAndBanks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "India", "IN" },
+                    { 2, "United Kingdom", "UK" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Banks",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Trivandrum Kerala", 1, "HDFC Bank", 4.5 },
+                    { 2, "Leamingtion Spa", 2, "HSBC Bank", 4.0 },
+                    { 3, "Leamingtion Spa", 2, "Barclays", 4.5 },
+                    { 4, "Athani, India", 1, "SBI", 4.7999999999999998 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Banks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Banks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Banks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Banks",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
